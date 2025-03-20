19b44808e9a06588f1b4bf5d1838be93b2afbc5b
susmitamenka <susmitaprasad73@gmail.com>
2025-03-21 00:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 //complete this code
 class Person {
-	static count=0
-	constructor(name,age){
-		this._name = name
-		this._age = age
-		Person.count++
-	}
-	getName(){
-		return this._name
-	}
-	setname(name){
-		this._name = name
-	}
-	getage(){
-		return this._age
-	}
-	setage(age){
-		this._age = age
-	}
+  static count = 0;
+
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+    Person.count++;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
 
-	study(){
-		console.log(`${this._name} is studying`)
-	}
+  study() {
+    console.log(${this._name} is studying);
+  }
 }
 
 class Teacher extends Person {
-	constructor(name,age){
-		super(name,age)
-	}
+  constructor(name, age) {
+    super(name, age);
+  }
 
-	teach(){
-		console.log(`${this._name} is teaching`)
-	}
+  teach() {
+    console.log(${this._name} is teaching);
+  }
 }
+
 
 // Do not change the code below this line
 window.Person = Person;
